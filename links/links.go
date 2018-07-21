2d8ac434967ed4d35ddc672f8c3c1a591c516542
krishna <krishnam.gubbi@gmail.com>
2018-07-21 19:07:21 +0530

    HTML links extractor package added

---
+++ links/links.go
@@ -0,0 +1,9 @@
+package links
+
+import (
+	"fmt"
+)
+
+func Extract(url string) {
+	fmt.Println("I shall extract the link here" + "--" + url)
+}
--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
+	"github.com/manjuk1/gocrawlweb/links"
 )
 
 func main(){
 	fmt.Println("Hello World! Starting a Web crawler project")
+	fmt.Println("Calling the Link Package Extract method")
+	links.Extract("hhtp://abc.com")
 }
